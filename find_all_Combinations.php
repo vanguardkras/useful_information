a334669c46540e771fba5061e386ba31032ed2ca
vanguardkras <championing@mail.ru>
2020-05-06 22:51:33 +0700

    Create find_all_Combinations.php

---
+++ find_all_Combinations.php
@@ -0,0 +1,10 @@
+function allCombinations(array $input_values)
+{
+    $combinations = [[]];
+
+    foreach ($input_values as $element) {
+        foreach ($combinations as $combination) {
+            array_push($combinations, array_merge($combination, [$element]));
+        }
+    }
+}
